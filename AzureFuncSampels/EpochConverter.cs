059a3ecf9bb07ff560742a623b56085cd9b42e46
sandeep <sandeepparekh9@gmail.com>
2018-06-28 16:59:04 +1000

    init

---
+++ AzureFuncSampels/EpochConverter.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace AzureFuncSampels
+{
+    public static class EpochConverter
+    {
+        private static readonly DateTime Epoch;
+
+        static EpochConverter()
+        {
+            Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        }
+
+        [FunctionName("EpochConverter")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get"
+            , Route = "epoch-convert/{value=value}")]HttpRequest req, double value, TraceWriter log)
+        {
+            var result = new List<KeyValue>();
+            var utcTime = Epoch.AddMilliseconds(value);
+            foreach (var tz in TimeZoneInfo.GetSystemTimeZones())
+            {
+                result.Add(new KeyValue
+                {
+                    Timezone = tz.DisplayName,
+                    Value = TimeZoneInfo.ConvertTimeFromUtc(utcTime, tz).ToString(CultureInfo.InvariantCulture)
+                });
+            }
+            return new OkObjectResult(result.OrderBy(x => x.Timezone));
+        }
+
+        private class KeyValue
+        {
+            public string Timezone { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
