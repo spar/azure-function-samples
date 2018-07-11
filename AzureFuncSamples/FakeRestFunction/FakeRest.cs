823f022d8ee78ebcbaab3a82274e9436a76f0f43
sandeep <sandeepparekh9@gmail.com>
2018-07-11 14:14:18 +1000

    simpler fakerest

--- AzureFuncSamples/FakeRestFunction/FakeRest.cs
+++ AzureFuncSamples/FakeRestFunction/FakeRest.cs
@@ -11,24 +11,68 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 
-namespace AzureFuncSamples
+namespace AzureFuncSamples.FakeRestFunction
 {
     public static class FakeRest
     {
         private static IEnumerable<User> _users;
 
-        private static void InitUsers(ExecutionContext context)
+        static FakeRest()
         {
-            if (_users != null) return;
-            var path = Path.Combine(context.FunctionAppDirectory, "Users.json");
-            _users = JsonConvert.DeserializeObject<IEnumerable<User>>(File.ReadAllText(path));
+            _users = new List<User>
+            {
+                new User
+                {
+                    Id= 1,
+                    FirstName= "Jaye",
+                    LastName="Tumility",
+                    Email="jtumility0@nature.com",
+                    Gender= "Male",
+                    Avatar= "https://robohash.org/velcommodisoluta.jpg?size=150x150&set=set1"
+                },
+                new User
+                {
+                    Id= 2,
+                    FirstName= "Winifield",
+                    LastName="Donat",
+                    Email="wdonat1@behance.net",
+                    Gender= "Male",
+                    Avatar= "https://robohash.org/velcommodisoluta.jpg?size=150x150&set=set1"
+                },
+                new User
+                {
+                    Id= 3,
+                    FirstName= "Deana",
+                    LastName="Ritmeier",
+                    Email="dritmeier2@booking.com",
+                    Gender= "Female",
+                    Avatar= "https://robohash.org/velcommodisoluta.jpg?size=150x150&set=set1"
+                },
+                new User
+                {
+                    Id= 4,
+                    FirstName= "Brade",
+                    LastName="Westwick",
+                    Email="bwestwick3@npr.org",
+                    Gender= "Male",
+                    Avatar= "https://robohash.org/velcommodisoluta.jpg?size=150x150&set=set1"
+                },
+                new User
+                {
+                    Id= 5,
+                    FirstName= "Marcille",
+                    LastName="Cinnamond",
+                    Email="mcinnamond4@icio.us",
+                    Gender= "Female",
+                    Avatar= "https://robohash.org/velcommodisoluta.jpg?size=150x150&set=set1"
+                }
+            };
         }
 
         [FunctionName("FakeRestUserGetAll")]
         public static IActionResult UserGetAll([HttpTrigger(AuthorizationLevel.Anonymous, "get"
             , Route = "fakerest/users")]HttpRequest req, TraceWriter log, ExecutionContext context)
         {
-            InitUsers(context);
             return new OkObjectResult(_users);
         }
 
@@ -36,7 +80,6 @@ namespace AzureFuncSamples
         public static IActionResult UserGet([HttpTrigger(AuthorizationLevel.Anonymous, "get"
             , Route = "fakerest/user/{id=id}")]HttpRequest req, int id, TraceWriter log, ExecutionContext context)
         {
-            InitUsers(context);
             return new OkObjectResult(_users?.FirstOrDefault(x => x.Id == id));
         }
 
@@ -46,7 +89,6 @@ namespace AzureFuncSamples
         {
             try
             {
-                InitUsers(context);
                 var reader = new StreamReader(req.Body, Encoding.UTF8);
                 var user = JsonConvert.DeserializeObject<User>(reader.ReadToEnd());
                 return new OkObjectResult(user);
@@ -63,7 +105,6 @@ namespace AzureFuncSamples
         {
             try
             {
-                InitUsers(context);
                 var reader = new StreamReader(req.Body, Encoding.UTF8);
                 var user = JsonConvert.DeserializeObject<User>(reader.ReadToEnd());
                 if (_users.Any(x => x.Id == user?.Id))
@@ -74,6 +115,13 @@ namespace AzureFuncSamples
             {
                 return new BadRequestObjectResult(ex.Message);
             }
+        }
+
+        [FunctionName("FakeRestUserDelete")]
+        public static IActionResult UserDelete([HttpTrigger(AuthorizationLevel.Anonymous, "delete"
+            , Route = "fakerest/user/{id=id}")]HttpRequest req, int id, TraceWriter log, ExecutionContext context)
+        {
+            return new OkResult();
         }
     }
 }
